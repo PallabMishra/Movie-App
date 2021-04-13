243dcb230e90aa988f888e0ba200e80689c168d1
Developer CSI <you@example.com>
2021-04-14 01:32:27 +0530

    initial commit

---
+++ app/src/main/java/com/pallab/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.pallab.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pallab.myapplication.models.Movie
+import com.pallab.myapplication.models.MovieResponse
+import com.pallab.myapplication.services.MovieApiInterface
+import com.pallab.myapplication.services.MovieApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rv_movies_list.layoutManager = LinearLayoutManager(this)
+        rv_movies_list.setHasFixedSize(true)
+        getMovieData { movies : List<Movie> ->
+            rv_movies_list.adapter = MovieAdapter(movies)
+        }
+    }
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                return callback(response.body()!!.movies)
+            }
+
+        })
+    }
+
+
+}
